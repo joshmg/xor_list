565cc61c202e7a35c171bedc62e2540b01006660
Joshua Green <josh@softwareverde.com>
2014-07-04 12:35:23 -0400

    Added a Last pointer for constant-time push.

--- xor_list.cpp
+++ xor_list.cpp
@@ -5,11 +5,11 @@ template <class T>
 class xor_list {
     private:
         struct xor_node {
-            mutable xor_node* address;
+            mutable const xor_node* address;
             T value;
 
             void print() const {
-                std::cout << value << std::endl;
+                std::cout << "(" << (unsigned long) this << ") " << value << " (" << (unsigned long) address << ")" << std::endl;
             }
         };
 
@@ -17,44 +17,58 @@ class xor_list {
             if (current_node->address == 0) return 0;
             else return (xor_node*) ((unsigned long) previous_node ^ (unsigned long) current_node->address);
         }
+        static const xor_node* get_prev(const xor_node* current_node, const xor_node* next_node) {
+            return (xor_node*) ((unsigned long) current_node->address ^ (unsigned long) next_node);
+        }
 
-        xor_node* _first;
+        const xor_node* _first_node;
+        const xor_node* _last_node;
 
     public:
         xor_list() {
-            _first = 0;
+            _first_node = 0;
+            _last_node = 0;
         }
 
-        void push(T item) {
-            xor_node* new_node = new xor_node();
-            new_node->value = item;
-
-            if (_first != 0) {
-                _first->print();
+        void print() const {
+            std::cout << "List:" << std::endl;
+            if (_first_node != 0) {
+                _first_node->print();
 
                 const xor_node* previous_node = 0;
-                const xor_node* current_node = _first;
-                while (current_node->address != 0) {
-                    // xor_node* next_node = (xor_node*) ((unsigned long) previous_node ^ (unsigned long) current_node->address);
+                const xor_node* current_node = _first_node;
+                do {
                     const xor_node* next_node = xor_list::get_next(previous_node, current_node);
 
                     previous_node = current_node;
                     current_node = next_node;
 
                     current_node->print();
-                }
-                current_node->address = (xor_node*) ((unsigned long) previous_node ^ (unsigned long) new_node);
-            }
-            else {
-                _first = new_node;
-                _first->address = 0;
+                } while (current_node != _last_node);
             }
 
-            new_node->print();
+            std::cout << std::endl;
+        }
+
+        void push(T item) {
+            xor_node* new_node = new xor_node();
+            new_node->value = item;
+            new_node->address = 0;
+
+            if (_last_node != 0) {
+                const xor_node* previous_node = xor_list::get_prev(_last_node, 0);
+
+                _last_node->address = (xor_node*) ((unsigned long) previous_node ^ (unsigned long) new_node);
+                new_node->address = _last_node;
+                _last_node = new_node;
+            }
+            else {
+                _first_node = new_node;
+                _last_node = _first_node;
+            }
         }
 
         T pop() {
-            
             return T();
         }
         T shift() {
@@ -62,39 +76,34 @@ class xor_list {
         }
 
         ~xor_list() {
-            if (_first == 0) {
-                return;
-            }
-            else {
-                const xor_node* current = _first;
-                const xor_node* next = xor_list::get_next(0, _first);
-                while (next != 0) {
-                    const xor_node* previous = current;
-                    current = next;
-                    next = xor_list::get_next(previous, current);
-                    delete previous;
-                    
-                }
-                delete current;
+            if (_first_node != 0) {
+                const xor_node* previous_node = 0;
+                const xor_node* current_node = _first_node;
+                do {
+                    const xor_node* next_node = xor_list::get_next(previous_node, current_node);
+
+                    previous_node = current_node;
+                    current_node = next_node;
+
+                    delete previous_node;
+                } while (current_node != _last_node);
             }
         }
 };
 
 int main() {
-    xor_list<int> list;
+    for (unsigned int i=0; i<1000; ++i) {
+        xor_list<int> list;
 
-    list.push(120);
-    std::cout << std::endl;
-    list.push(121);
-    std::cout << std::endl;
-    list.push(122);
-    std::cout << std::endl;
-    list.push(123);
-    std::cout << std::endl;
-    list.push(124);
-    std::cout << std::endl;
-    list.push(125);
-    std::cout << std::endl;
+        list.push(120);
+        list.push(121);
+        list.push(122);
+        list.push(123);
+        list.push(124);
+        list.push(125);
+
+        list.print();
+    }
 
     return 0;
 }
